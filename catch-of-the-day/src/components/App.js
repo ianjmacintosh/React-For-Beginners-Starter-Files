a2d4c2e41fb5a8b22c062f80329aff3e573bc18b
Ian MacIntosh <ianjmacintosh@gmail.com>
2021-03-30 14:59:33 -0300

    Apply Prettier fixes

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,15 +5,15 @@ import Inventory from "./Inventory";
 import Order from "./Order";
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="catch-of-the-day">
-                <Header tagline="Fresh Seafood Market" />
-                <Inventory></Inventory>
-            <Order></Order>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <Header tagline="Fresh Seafood Market" />
+        <Inventory></Inventory>
+        <Order></Order>
+      </div>
+    );
+  }
 }
 
 export default App;
